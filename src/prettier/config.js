02b546dd796d226f38848e3924e8bece250d5f1a
Nick Nisi <nick@nisi.org>
2023-12-30 13:08:04 -0600

    setup tsconfig and prettier configs

---
+++ src/prettier/config.js
@@ -0,0 +1,10 @@
+/** @type import('prettier').Config */
+export default {
+	useTabs: true,
+	tabWidth: 2,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	arrowParens: 'avoid',
+	printWidth: 120,
+};
